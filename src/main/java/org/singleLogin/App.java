620657dd0f67a6897ffcbf495d14a8f851f76fc8
Luer123 <2701569156@qq.com>
2025-03-18 14:52:01 +0800

    更新输出信息，添加 "Hello World!顶顶顶"

--- src/main/java/org/singleLogin/App.java
+++ src/main/java/org/singleLogin/App.java
@@ -9,6 +9,8 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!123776" );
-        System.out.println( "Hello World!161" );
+        System.out.println("Hello World!161");
+        
+        System.out.println("Hello World!顶顶顶");
     }
 }
